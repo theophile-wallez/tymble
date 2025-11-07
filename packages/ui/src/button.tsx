fd3b218e5a435b136a0c43aef8562954c1b14a23
Théophile Wallez <theophile.wall@gmail.com>
2025-11-07 22:20:39 +0100

    init

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
-interface ButtonProps {
+type ButtonProps = {
   children: ReactNode;
   className?: string;
   appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
 };
+
+export const Button = ({ children, className, appName }: ButtonProps) => (
+  <button
+    className={className}
+    onClick={() => console.log(`Hello from your ${appName} app!`)}
+    type="button"
+  >
+    {children}
+  </button>
+);
