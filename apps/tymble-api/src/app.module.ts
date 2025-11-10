9415227974a10a1ccced12589457ee0d8cc5731a
Théophile Wallez <theophile.wall@gmail.com>
2025-11-11 00:13:02 +0100

    Remove Prisma

--- apps/tymble-api/src/app.module.ts
+++ apps/tymble-api/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 import { SupabaseService } from './supabase/supabase.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService, PrismaService, SupabaseService],
+  providers: [AppService, SupabaseService],
 })
 export class AppModule {}
