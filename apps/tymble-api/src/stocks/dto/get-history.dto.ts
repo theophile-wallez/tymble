a22d02408dcb62ce79a0e85defec65d9e9f6179c
Théophile Wallez <theophile.wall@gmail.com>
2025-11-15 02:06:00 +0100

    zod validation

--- apps/tymble-api/src/stocks/dto/get-history.dto.ts
+++ apps/tymble-api/src/stocks/dto/get-history.dto.ts
@@ -1,32 +1,28 @@
-import { IsIn, IsOptional, IsString } from 'class-validator';
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
 import { ChartOptions } from 'yahoo-finance2/modules/chart';
 
-export class GetChartDto {
-  @IsString() ticker!: string;
+const intervals = [
+  '1m',
+  '2m',
+  '5m',
+  '15m',
+  '30m',
+  '60m',
+  '90m',
+  '1h',
+  '1d',
+  '5d',
+  '1wk',
+  '1mo',
+  '3mo',
+] as const satisfies ChartOptions['interval'][];
 
-  @IsOptional()
-  @IsString()
-  period1?: ChartOptions['period1'];
+const GetChartSchema = z.object({
+  ticker: z.string(),
+  period1: z.string().optional(),
+  period2: z.string().optional(),
+  interval: z.enum(intervals).optional(),
+});
 
-  @IsOptional()
-  @IsString()
-  period2?: ChartOptions['period2'];
-
-  @IsOptional()
-  @IsIn([
-    '1m',
-    '2m',
-    '5m',
-    '15m',
-    '30m',
-    '60m',
-    '90m',
-    '1h',
-    '1d',
-    '5d',
-    '1wk',
-    '1mo',
-    '3mo',
-  ])
-  interval?: ChartOptions['interval'];
-}
+export class GetChartDto extends createZodDto(GetChartSchema) {}
