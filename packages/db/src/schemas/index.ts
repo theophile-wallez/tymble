41a3b3cde0f4551170a76f280d74902c014d6d67
Théophile Wallez <theophile.wall@gmail.com>
2025-11-16 20:02:35 +0100

    wip

--- packages/db/src/schemas/index.ts
+++ packages/db/src/schemas/index.ts
@@ -1,5 +1,4 @@
 export * from './instrument.schema';
-export * from './language/language.schema';
 export * from './portfolio.schema';
 export * from './transaction.schema';
 export * from './user/user.schema';
