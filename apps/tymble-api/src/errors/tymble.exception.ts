4f927af148ea66a4fa127ab133cdef73b534d3e4
Théophile Wallez <theophile.wall@gmail.com>
2025-11-23 00:51:06 +0100

    Create drizzle.exception.ts

---
+++ apps/tymble-api/src/errors/tymble.exception.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class TymbleException extends HttpException {
+  constructor(
+    code: string,
+    message: string,
+    status: HttpStatus,
+    context?: Record<string, unknown>
+  ) {
+    const response = {
+      code,
+      message,
+      timestamp: new Date().toISOString(),
+      context,
+    };
+
+    super(response, status);
+  }
+}
