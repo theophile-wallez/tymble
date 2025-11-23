978617a865e37944c36fcf171023e327f67bac24
Théophile Wallez <theophile.wall@gmail.com>
2025-11-23 02:07:04 +0100

    Add react query

--- apps/tymble-ui/src/App.tsx
+++ apps/tymble-ui/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 
-function App() {
+export const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <p />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
-}
-
-export default App;
+};
