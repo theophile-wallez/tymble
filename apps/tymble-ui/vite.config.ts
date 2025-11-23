aebf1d53c23a92e012f03c19c8a27a892ac25713
Théophile Wallez <theophile.wall@gmail.com>
2025-11-23 01:33:13 +0100

    Create button.tsx

--- apps/tymble-ui/vite.config.ts
+++ apps/tymble-ui/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
