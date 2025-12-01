34ec050f807695e6dd784bdf58ab37782a471161
Théophile Wallez <theophile.wall@gmail.com>
2025-12-01 21:30:32 +0100

    🎉 feat: add @tanstack/router-plugin to enhance routing capabilities and update Vite configuration for improved plugin integration

--- apps/tymble-ui/vite.config.ts
+++ apps/tymble-ui/vite.config.ts
@@ -1,11 +1,19 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
