a9685bf08dbb74f9f09d34888c11f115216a5ddc
Théophile Wallez <theophile.wall@gmail.com>
2025-12-01 22:08:48 +0100

    🎨 style: refactor RootLayout to conditionally render AppLayout based on login route

--- apps/tymble-ui/src/routes/__root.tsx
+++ apps/tymble-ui/src/routes/__root.tsx
@@ -1,12 +1,17 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useRouterState,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-const RootLayout = () => (
+const AppLayout = () => (
   <>
     <div className="flex gap-2 p-2">
       <Link className="[&.active]:font-bold" to="/">
         Home
-      </Link>{' '}
+      </Link>
       <Link className="[&.active]:font-bold" to="/about">
         About
       </Link>
@@ -16,8 +21,21 @@ const RootLayout = () => (
     </div>
     <hr />
     <Outlet />
-    <TanStackRouterDevtools />
   </>
 );
+
+const RootLayout = () => {
+  const pathname = useRouterState({
+    select: (state) => state.location.pathname,
+  });
+  const isLoginRoute = pathname === '/login';
+
+  return (
+    <>
+      {isLoginRoute ? <Outlet /> : <AppLayout />}
+      <TanStackRouterDevtools />
+    </>
+  );
+};
 
 export const Route = createRootRoute({ component: RootLayout });
