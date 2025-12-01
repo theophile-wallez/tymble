2f4678a85b9b8577e64312119ba136c750d04867
Théophile Wallez <theophile.wall@gmail.com>
2025-12-01 22:01:57 +0100

    🎨 style: ensure index.css is imported in main.tsx

--- apps/tymble-ui/src/main.tsx
+++ apps/tymble-ui/src/main.tsx
@@ -3,6 +3,7 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
