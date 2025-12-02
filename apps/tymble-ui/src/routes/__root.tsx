b8db0b2b726165a65faca5ad2fd5735aa5095a9b
Théophile Wallez <theophile.wall@gmail.com>
2025-12-03 00:34:38 +0100

    refactor: remove App component

--- apps/tymble-ui/src/routes/__root.tsx
+++ apps/tymble-ui/src/routes/__root.tsx
@@ -1,9 +1,9 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ThemeProvider } from '@/components/theme/theme-provider';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from '@/components/theme/theme-provider';
 
 const queryClient = new QueryClient();
 
