ca4a85e133adb8aa8d2dff2298f8cd7b7704e60b
theophilewallez <66305625+theophile-wallez@users.noreply.github.com>
2025-12-07 23:32:54 +0100

    feat: Add react-grid-layout dependency and refactor content layout to include header and title components.

--- apps/tymble-ui/src/components/dashboards/dashboards.tsx
+++ apps/tymble-ui/src/components/dashboards/dashboards.tsx
@@ -1,11 +1,17 @@
 import { useTranslation } from '@/hooks/use-translation';
-import { ContentLayout } from '@/layouts/content.layout';
+import {
+  ContentHeader,
+  ContentLayout,
+  ContentTitle,
+} from '@/layouts/content.layout';
 
 export const Dashboards = () => {
   const { t } = useTranslation();
   return (
-    <ContentLayout pageId="dashboards">
-      <h1>My Dashboards</h1>
+    <ContentLayout cy="dashboards">
+      <ContentHeader cy="dashboards">
+        <ContentTitle cy="dashboards">Dashboards</ContentTitle>
+      </ContentHeader>
     </ContentLayout>
   );
 };
--- apps/tymble-ui/src/layouts/content.layout.tsx
+++ apps/tymble-ui/src/layouts/content.layout.tsx
@@ -2,14 +2,33 @@ import type React from 'react';
 import { cn } from '@/ui/utils';
 
 type Props = React.HTMLAttributes<HTMLDivElement> & {
-  pageId?: string;
+  cy?: string;
 };
 
-export const ContentLayout = ({ className, pageId, ...props }: Props) => (
+export const ContentLayout = ({ className, cy, ...props }: Props) => (
   <section
     {...props}
-    className={cn('flex size-full flex-col', className)}
-    data-cy={`content-layout-${pageId}`}
-    id={pageId}
+    className={cn('relative flex size-full flex-col', className)}
+    data-cy={`content-layout-${cy}`}
+    id={cy}
+  />
+);
+
+export const ContentHeader = ({ className, cy, ...props }: Props) => (
+  <header
+    {...props}
+    className={cn(
+      'flex h-12 w-full items-center justify-between border-border border-b bg-accent px-6',
+      className
+    )}
+    data-cy={`content-header-${cy}`}
+  />
+);
+
+export const ContentTitle = ({ className, cy, ...props }: Props) => (
+  <h1
+    {...props}
+    className={cn('font-normal text-base', className)}
+    data-cy={`content-title-${cy}`}
   />
 );
